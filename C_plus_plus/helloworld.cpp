58b57e410317b5fbddd387315537dc9507a03349
MbabaziOwen <163897840+MbabaziOwen@users.noreply.github.com>
2026-01-30 11:19:44 +0300

    the start of C++

---
+++ C_plus_plus/helloworld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    // this is a comment 
+    /*
+    this 
+    is 
+    a 
+    anothrer 
+    comment 
+    
+    */
+    std::cout << "i like pizza!" << std::endl;
+    std:: cout << "its really good" << std::endl;
+    return 0;
+}
