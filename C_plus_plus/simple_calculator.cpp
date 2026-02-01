8d4c98a45651260492d28fdb0426268b386955b4
MbabaziOwen <163897840+MbabaziOwen@users.noreply.github.com>
2026-02-02 01:14:43 +0300

    calculator update

--- C_plus_plus/simple_calculator.cpp
+++ C_plus_plus/simple_calculator.cpp
@@ -21,19 +21,30 @@ int main(){
         case 'a': //addition
         answer = first_value+second_value;
         std::cout<<"the addition of the values is "<< answer;
+        break;
 
-        
+        case 'd': //division
+        if(second_value==0){
+            std::cout<<"you can not divide by zero ";
+            break;
+        }else{
+            answer = first_value/second_value;
+            std::cout<<"the division of the value "<< answer;
+            break;
+        }
         
 
         case 's': //subtraction
         answer = first_value-second_value;
         std::cout<<"the subtraction of the values is "<< answer;
+        break;
 
         case 'm': //multiplication
         answer = first_value*second_value;
         std::cout<<"the multiplication of the values is "<< answer;
-
         break;
+
+        
         default:
             std::cout<<"enter the right options ";
         
