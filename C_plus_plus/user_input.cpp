2af1a012c23d5cd4c385555b8b47a336756d2b67
MbabaziOwen <163897840+MbabaziOwen@users.noreply.github.com>
2026-02-01 22:00:03 +0300

    cin and cout

---
+++ C_plus_plus/user_input.cpp
@@ -0,0 +1,22 @@
+//cout << (insertion operator)
+// cin >> (extraction operator)
+
+#include<iostream>
+int main(){
+
+    //asking user for their name 
+    std::string name;
+    std::cout<<"what is your name: ";
+    std::getline(std::cin,name);
+
+   
+      
+     
+    //asking user for their age 
+    int age;
+    std::cout<<"what is your age? ";
+    std::cin>>age;
+     std::cout<<"hello "<< name<<"\n";
+    std::cout<<"you are "<<age<<" years old";
+    return 0;
+}
