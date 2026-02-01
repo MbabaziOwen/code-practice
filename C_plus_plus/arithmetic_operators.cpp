8d150ddcdb1b6dcb81fa6e5f4d565e1f65ee3ec5
MbabaziOwen <163897840+MbabaziOwen@users.noreply.github.com>
2026-02-01 21:30:31 +0300

    arithmetic operators

---
+++ C_plus_plus/arithmetic_operators.cpp
@@ -0,0 +1,12 @@
+// arithmetic operators = return results of a specific artihmetic operation 
+
+#include <iostream>
+int main(){
+    int student =20;
+
+    student+=1;
+     
+    std::cout << student;
+    return 0;
+}
+
