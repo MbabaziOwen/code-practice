4c8a6f344d8769e622a8a3298cbb94f2ff47e231
MbabaziOwen <163897840+MbabaziOwen@users.noreply.github.com>
2026-02-02 01:12:45 +0300

    simple calculator

---
+++ C_plus_plus/simple_calculator.cpp
@@ -0,0 +1,46 @@
+//this is a simple calculator program 
+//it can add divide and subtract 
+
+#include<iostream>
+#include<cmath>
+
+int main(){
+    char operators;
+    double first_value;
+    double second_value;
+    double answer;
+
+    std::cout<<"enter the first value and second value; \n";
+    std::cin>>first_value;
+    std::cin>>second_value;
+
+    std::cout<<"enter operand A for addition , D for division or S for subtruction ";
+    std::cin>>operators;
+
+    switch(operators){
+        case 'a': //addition
+        answer = first_value+second_value;
+        std::cout<<"the addition of the values is "<< answer;
+
+        
+        
+
+        case 's': //subtraction
+        answer = first_value-second_value;
+        std::cout<<"the subtraction of the values is "<< answer;
+
+        case 'm': //multiplication
+        answer = first_value*second_value;
+        std::cout<<"the multiplication of the values is "<< answer;
+
+        break;
+        default:
+            std::cout<<"enter the right options ";
+        
+
+    }
+
+
+
+    return 0;
+}
