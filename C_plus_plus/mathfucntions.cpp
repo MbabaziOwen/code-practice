9e5331420f3b26767ac6801e8b689ff3e56f92fd
MbabaziOwen <163897840+MbabaziOwen@users.noreply.github.com>
2026-02-01 22:16:15 +0300

    math functions

---
+++ C_plus_plus/mathfucntions.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+//man need the cmath function 
+#include<cmath>
+int main(){
+    double x = 3;
+    double y = 4;
+    double z;
+
+    //z = std::max(x,y); //finding the max 
+    //z = std::min(x,y); //finding the min 
+
+    //z = pow(2,4);
+    //z = sqrt(2,4);
+
+    //z = abs(-3);//for the absolute value
+
+    //z=round(x);//rounding off a number
+    //z= ceil(x); //rounding up the number
+    //z=floor(x); //rounding down 
+
+    std::cout<<z;
+
+
+    return 0;
+}
