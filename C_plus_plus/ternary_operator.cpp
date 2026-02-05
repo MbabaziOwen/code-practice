765941daa17dffe6898507894770eff5fd7a5782
MbabaziOwen <163897840+MbabaziOwen@users.noreply.github.com>
2026-02-05 17:15:44 +0300

    ternary operator

---
+++ C_plus_plus/ternary_operator.cpp
@@ -0,0 +1,18 @@
+//ternary operator = replacemtent to an if/else statement 
+// condition? expression1 : expression2;
+
+#include<iostream>
+int main(){
+    
+     //int grade = 75;
+    // grade >= 60? std::cout <<"you pass" : std::cout<<"you fail"; 
+
+    //int num = 2;
+    //num%2 == 0 ? std::cout <<"it is even " : std::cout<<"it is odd";
+
+    bool hungry = true;
+    hungry? std::cout<<"you are houngry ": std::cout<<"you are full";
+
+
+    return 0;
+} 
