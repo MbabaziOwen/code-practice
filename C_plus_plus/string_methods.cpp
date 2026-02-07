80806b301a603fd7d819a61fb9739366cfd0af2b
MbabaziOwen <163897840+MbabaziOwen@users.noreply.github.com>
2026-02-08 01:04:30 +0300

    strings

--- C_plus_plus/string_methods.cpp
+++ C_plus_plus/string_methods.cpp
@@ -6,12 +6,25 @@ int main(){
 
     std::getline(std::cin,name);
 
-    if(name.length() > 12){
-        std::cout<<"your name cant be over 12 characters";
+    //if(name.length() > 12){  //for checking the length of the text 
+       // std::cout<<"your name cant be over 12 characters";
 
-    }else{
-        std::cout<<"welcome "<<name;
-    }
+    //}else{
+        //std::cout<<"welcome "<<name;
+   // }
+
+    //if(name.empty()){ //checking if the input is empty
+       //std::cout<<"you didnt enter you name";
+
+    //}else{
+        //std::cout<<"welcome "<<name;
+    //}
+
+    //name.append("@gmail.com");
+    //std::cout<<"you username is now "<<name;
+
+    std::cout<<name.at(0);
+    
 
     return 0;
 }
